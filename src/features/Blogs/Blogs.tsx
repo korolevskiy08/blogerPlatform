cd979695af5cd77e6fb609ca67626dc92f06ac77
korolevskiy08 <emkorolev12@gmail.com>
2022-11-10 22:17:49 +0300

    feat: add style Blog

--- src/features/Blogs/Blogs.tsx
+++ src/features/Blogs/Blogs.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 
+import imageSvg from '../../common/icons/Image.svg';
 import style from '../../layout/global.module.css';
 
 import styles from './Blogs.module.css';
@@ -9,21 +10,23 @@ export const Blogs: FC = () => {
   return (
     <div className={styles.blogsBlock}>
       <div className={style.container}>
-        <div className={styles.titleBlock}>
+        <div className={styles.titleBlogs}>
           <h2 className={style.title}>Blogs</h2>
         </div>
         <Settings />
 
         <div className={styles.blogBlock}>
           <div className={styles.avatarBlogs}>
-            <img alt="" />
+            <div>
+              <img src={imageSvg} alt="avatar" className={styles.avatar} />
+            </div>
           </div>
           <div className={styles.descriptionBlock}>
-            <div>
+            <div className={styles.titleBlock}>
               <h3 className={styles.titleBlog}>The best blog in our village</h3>
             </div>
             <div>
-              <p>
+              <p className={styles.descriptionText}>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci in quas
                 qui sint. Accusantium cum error hic, magnam nisi numquam officia quaerat
                 quam quidem tempora! Cumque eum modi voluptas voluptate.
