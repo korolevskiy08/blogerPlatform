40af3365222d1889a3aae8de1713267f8c59aa3d
korolevskiy08 <emkorolev12@gmail.com>
2022-11-14 23:12:27 +0300

    feat: add loader

--- src/features/Blogs/Blogs.tsx
+++ src/features/Blogs/Blogs.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useEffect } from 'react';
 
+import { CircularProgress } from '@mui/material';
+
 import { useAppDispatch } from '../../common/hooks/useAppDispatch';
 import { useAppSelector } from '../../common/hooks/useAppSelector';
 import { Button } from '../../layout/Button/Button';
@@ -25,17 +27,25 @@ export const Blogs: FC = () => {
           <h2 className={style.title}>Blogs</h2>
         </div>
         <Settings />
-        {blogs.blogs.map(el => {
-          return (
-            <Blog
-              name={el.name}
-              key={el.id}
-              id={el.id}
-              description={el.youtubeUrl}
-              buttonComponent={<Button onclick={() => {}} title="Show more" />}
-            />
-          );
-        })}
+        {blogs.status === 'loading' ? (
+          <div className={styles.loader}>
+            <CircularProgress color="inherit" />
+          </div>
+        ) : (
+          <div>
+            {blogs.blogs.map(el => {
+              return (
+                <Blog
+                  name={el.name}
+                  key={el.id}
+                  id={el.id}
+                  description={el.youtubeUrl}
+                  buttonComponent={<Button onclick={() => {}} title="Show more" />}
+                />
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
