e380c878978b938fd9a3d5de38289fd0bb407963
korolevskiy08 <emkorolev12@gmail.com>
2022-11-15 19:08:03 +0300

    feat: add posts styles

--- src/features/Posts/Posts.tsx
+++ src/features/Posts/Posts.tsx
@@ -1,7 +1,11 @@
 import React, { FC, useEffect } from 'react';
 
+import { CircularProgress } from '@mui/material';
+
 import { useAppDispatch } from '../../common/hooks/useAppDispatch';
 import { useAppSelector } from '../../common/hooks/useAppSelector';
+import { Button } from '../../layout/Button/Button';
+import style from '../../layout/global.module.css';
 import { TitleComponent } from '../../layout/TitleComponent/TitleComponent';
 import Select from '../Blogs/Settings/Select/Select';
 
@@ -13,7 +17,6 @@ export const Posts: FC = () => {
   const dispatch = useAppDispatch();
   const posts = useAppSelector(state => state.posts);
 
-  console.log(posts);
   useEffect(() => {
     dispatch(getPosts());
   }, []);
@@ -24,16 +27,34 @@ export const Posts: FC = () => {
       <div className={styles.selectBlock}>
         <Select />
       </div>
-      {posts.posts.map(el => {
-        return (
-          <Post
-            key={el.id}
-            blogName={el.blogName}
-            content={el.content}
-            createdAt={el.createdAt}
-          />
-        );
-      })}
+      {posts.status === 'loading' ? (
+        <div className={style.loader}>
+          <CircularProgress color="inherit" />
+        </div>
+      ) : (
+        <ul className={styles.postContainer}>
+          {posts.posts.map(el => {
+            return (
+              <li key={el.id}>
+                <Post
+                  key={el.id}
+                  blogName={el.blogName}
+                  content={el.content}
+                  createdAt={el.createdAt}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+
+      <div className={style.buttonBlock}>
+        <Button
+          title="Show more"
+          onclick={() => {}}
+          styleButton={styles.buttonShowMore}
+        />
+      </div>
     </div>
   );
 };
