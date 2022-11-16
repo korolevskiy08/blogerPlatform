82a1893e7cd37ccf7d30ac3860345a9cf26fc07a
korolevskiy08 <emkorolev12@gmail.com>
2022-11-16 22:03:37 +0300

    feat: edit styles from Blogs

--- src/features/BlogItem/BlogItem.tsx
+++ src/features/BlogItem/BlogItem.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useEffect } from 'react';
 
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { useAppDispatch } from '../../common/hooks/useAppDispatch';
 import { useAppSelector } from '../../common/hooks/useAppSelector';
 import arrowLeft from '../../common/icons/arrow-left.svg';
 import arrowRight from '../../common/icons/arrow_right.svg';
-import imageTitle from '../../common/icons/Image.svg';
+import imageTitle from '../../common/images/blue-ocean-28668-2560x1600.jpg';
+import { Path } from '../../common/Routes';
 import { Button } from '../../layout/Button/Button';
 import style from '../../layout/global.module.css';
 import Blog from '../Blogs/Blog/Blog';
@@ -18,20 +19,28 @@ export const BlogItem: FC = () => {
   const { blogId } = useParams();
   const blog = useAppSelector(state => state.blog.blog);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (blogId) dispatch(getBlog(blogId));
   }, []);
 
+  const navigateBlogs = (event: React.MouseEvent<HTMLElement>): void => {
+    event.stopPropagation();
+    navigate(Path.Blogs);
+  };
+
   return (
     <div className={styles.blogBlock}>
       <div className={styles.containerBlog}>
         <div className={style.titleBlock}>
-          <h2 className={style.titleBlog}>Blogs</h2>
+          <h2 className={style.titleBlog} role="presentation" onClick={navigateBlogs}>
+            Blogs
+          </h2>
           <img src={arrowRight} alt="arrowRight" />
           <h3 className={style.titleName}>{blog.name}</h3>
         </div>
-        <div className={styles.backBlogs}>
+        <div className={styles.backBlogs} role="presentation" onClick={navigateBlogs}>
           <img src={arrowLeft} alt="arrowLeft" />
           <p className={styles.backBlogsText}>Back to blogs</p>
         </div>
--- src/features/Blogs/Blog/Blog.tsx
+++ src/features/Blogs/Blog/Blog.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
-import imageSvg from '../../../common/icons/Image.svg';
+import imageBlog from '../../../common/images/Gull_portrait_ca_usa.jpg';
 
 import styles from './Blog.module.css';
 
@@ -25,29 +25,19 @@ const Blog: FC<BlogType> = ({ name, id, description, date }) => {
     <div className={styles.blogBlock}>
       <div className={styles.blogItem}>
         <div className={styles.avatarBlogs}>
-          <div>
-            <img src={imageSvg} alt="avatar" className={styles.avatar} />
-          </div>
+          <img src={imageBlog} alt="avatar" />
         </div>
         <div className={styles.descriptionBlock}>
-          <div className={styles.titleBlock}>
-            <h3
-              onClick={navigateBlogItem}
-              role="presentation"
-              className={styles.titleBlog}
-            >
-              {name}
-            </h3>
-          </div>
-          <div>
-            {date ? (
-              <div>
-                <p>Blog creation date: {date?.slice(0, 10)}</p>
-              </div>
-            ) : (
-              ''
-            )}
-          </div>
+          <h3 onClick={navigateBlogItem} role="presentation" className={styles.titleBlog}>
+            {name}
+          </h3>
+          {date ? (
+            <div>
+              <p>Blog creation date: {date?.slice(0, 10)}</p>
+            </div>
+          ) : (
+            ''
+          )}
           <div className={styles.youTubeBlock}>
             <p className={styles.youTube}>YouTube:</p> {description}
           </div>
