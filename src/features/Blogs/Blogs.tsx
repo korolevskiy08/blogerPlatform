cbe857259ae868d7b2385517f72ab9c55b0b6de4
korolevskiy08 <emkorolev12@gmail.com>
2022-12-12 13:08:12 +0300

    feat: pagination dynamic

--- src/features/Blogs/Blogs.tsx
+++ src/features/Blogs/Blogs.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { CircularProgress } from '@mui/material';
 
@@ -10,6 +10,8 @@ import style from '../../layout/global.module.css';
 
 import BlogItem from './BlogItem/BlogItem';
 import { getBlogs } from './blogs-actions';
+import { BlogType } from './blogs-api';
+import { setFilterBlogs } from './blogs-slice';
 import styles from './blogs.module.css';
 import { FilterBlock } from './FilterBlock/FilterBlock';
 
@@ -17,9 +19,45 @@ export const Blogs: FC = () => {
   const dispatch = useAppDispatch();
   const blogs = useAppSelector(state => state.blogs);
 
+  const [currentBlogs, setCurrentBlogs] = useState<BlogType[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fetching, setFetching] = useState(true);
+  const [totalCount, setTotalCount] = useState(0);
+  const num = 100;
+
   useEffect(() => {
-    dispatch(getBlogs());
-  }, []);
+    if (fetching) {
+      dispatch(setFilterBlogs({ pageNumber: currentPage }));
+      dispatch(getBlogs())
+        .then((res: any) => {
+          setCurrentBlogs([...currentBlogs, ...res.payload.data.items]);
+          setCurrentPage(prevState => prevState + 1);
+          setTotalCount(res.payload.data.totalCount);
+        })
+        .finally(() => {
+          setFetching(false);
+        });
+    }
+  }, [fetching]);
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+
+    return () => {
+      document.removeEventListener('scroll', scrollHandler);
+    };
+  }, [totalCount]);
+
+  const scrollHandler = (e: any): void => {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+        num &&
+      currentBlogs.length < totalCount
+    ) {
+      setFetching(true);
+    }
+  };
 
   return (
     <div className={styles.blogsBlock}>
@@ -27,31 +65,30 @@ export const Blogs: FC = () => {
         <TitleComponent title="Blogs" />
         <FilterBlock searchNameTerm={blogs.params.searchNameTerm} />
       </div>
-      {blogs.status === 'loading' ? (
-        <div className={style.loader}>
-          <CircularProgress color="inherit" />
-        </div>
-      ) : (
-        <div>
-          {blogs.blogs.map(el => {
-            return (
-              <BlogItem
-                name={el.name}
-                key={el.id}
-                id={el.id}
-                websiteUrl={el.websiteUrl}
-                description={el.description}
-              />
-            );
-          })}
-          <div className={style.buttonBlock}>
-            <div className={styles.buttonShowMore}>
-              Show more
-              <ArrowBottom className={styles.arrowBottom} />
-            </div>
+      <div>
+        {currentBlogs.map(el => {
+          return (
+            <BlogItem
+              name={el.name}
+              key={el.id}
+              id={el.id}
+              websiteUrl={el.websiteUrl}
+              description={el.description}
+            />
+          );
+        })}
+        {blogs.status === 'loading' && (
+          <div className={style.loader}>
+            <CircularProgress color="inherit" />
+          </div>
+        )}
+        <div className={style.buttonBlock}>
+          <div className={styles.buttonShowMore}>
+            Show more
+            <ArrowBottom className={styles.arrowBottom} />
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
