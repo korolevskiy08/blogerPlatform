0fccd904003d3f56e71cd5d8cec636a4c2bf7035
korolevskiy08 <emkorolev12@gmail.com>
2023-01-12 22:35:00 +0300

    fix: refactoring link

--- src/features/auth/Congratulation/Congratulation.tsx
+++ src/features/auth/Congratulation/Congratulation.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import { Button } from '../../../common/Components/Button/Button';
 import { Wrapper } from '../../../common/Components/Wrapper/Wrapper';
 import bro from '../../../common/images/bro.png';
@@ -8,13 +10,19 @@ import style from '../../../layout/global.module.css';
 import styles from './congratulation.module.css';
 
 export const Congratulation: FC = () => {
+  const navigate = useNavigate();
+
+  const navigateSignIn = (): void => {
+    navigate('/SignIn');
+  };
+
   return (
     <Wrapper showNavigation={false}>
       <div className={styles.container}>
         <h2 className={`${style.textGlobal} ${styles.title}`}>
           Congratulations! Your email has been confirmed
         </h2>
-        <Button styleButton={style.button} onclick={() => {}}>
+        <Button styleButton={style.button} onclick={navigateSignIn}>
           Sign In
         </Button>
         <img src={bro} alt="bro" />
