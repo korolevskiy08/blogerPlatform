9b3a2bc5d7687ba29975966b5d7b70e424f3bc5e
korolevskiy08 <emkorolev12@gmail.com>
2023-01-13 16:51:44 +0300

    feat: refactoring link

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,18 @@ import React, { FC, useEffect } from 'react';
 
 import '../App.css';
 
+import { useLocation } from 'react-router-dom';
+
 import { useAppDispatch } from '../common/hooks/useAppDispatch';
 import { userData } from '../features/auth/auth-actions';
 
 import { AppRoutes } from './AppRoutes/AppRoutes';
 
 const App: FC = () => {
+  const location = useLocation();
+
+  console.log(location);
+
   const dispatch = useAppDispatch();
 
   useEffect(() => {
--- src/features/auth/auth-api.ts
+++ src/features/auth/auth-api.ts
@@ -16,7 +16,7 @@ export const authApi = {
   signUp(data: SignUpType) {
     return instance.post('auth/registration', {
       ...data,
-      link: process.env.LINK,
+      link: process.env.REACT_APP_LINK,
     });
   },
   logout() {
