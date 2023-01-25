eccbaf3163918f4af8be53f686bde5396dc51970
korolevskiy08 <emkorolev12@gmail.com>
2023-01-25 15:42:44 +0300

    fix: fix validate for new password

--- src/common/function/validateNewPassword.ts
+++ src/common/function/validateNewPassword.ts
@@ -11,10 +11,10 @@ export const validateNewPassword = (values: any): NewPasswordFormType => {
     errors.newPassword = 'Password must be between 2 and 6 characters';
   }
 
-  if (!values.recoveryCode) {
-    errors.recoveryCode = 'Required';
-  } else if (values.newPassword !== values.recoveryCode) {
-    errors.recoveryCode = 'Passwords do not match';
+  if (!values.passwordConfirmation) {
+    errors.passwordConfirmation = 'Required';
+  } else if (values.newPassword !== values.passwordConfirmation) {
+    errors.passwordConfirmation = 'Passwords do not match';
   }
 
   return errors;
